c6391e94bf55cfad6a0ad7d896e7c17e51052465
chenshuojia <chensj@tututech.net>
2017-08-24 18:17:34 +0800

    some update

--- server/middleware/router.js
+++ server/middleware/router.js
@@ -1,11 +1,11 @@
 import Router from 'koa-router'
 import config from '../config'
 import sha1 from 'sha1'
-import '../wechat'
 
 export const router = app => {
     const router = new Router()
     router.get('/wechat-hear', (ctx, next) => {
+        require('../wechat') //项目启动之后再引入
         const token = config.wechat.token
         const {signature, nonce, timestamp, echostr}
             = ctx.query
